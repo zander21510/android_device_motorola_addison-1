700656f101258e8397af717d766fb87117553593
BtbN <btbn@btbn.de>
2018-08-14 01:38:29 +0200

    Update DRM / DRM HIDL
    Change-Id: Ieb391b90ad16b5a1431ef568c67fcee36d683670

--- device.mk
+++ device.mk
@@ -138,6 +138,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-service.widevine \
     libprotobuf-cpp-lite
 
 # Ebtables
