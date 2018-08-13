9892981f2df56941f3fda73bbc8148e8c82f92ff
BtbN <btbn@btbn.de>
2018-08-14 01:41:46 +0200

    Update Bluetooth services
    Change-Id: I2e8c3263c65561b7c0ebe5c00ed88099c2aa025a

--- device.mk
+++ device.mk
@@ -99,7 +99,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    libbt-vendor
+    libbt-vendor \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
