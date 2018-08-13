d4dcc84dd71bb3f97560afc4c28df8787ea10619
BtbN <btbn@btbn.de>
2018-08-13 19:19:36 +0200

    Build LiveDisplay HAL
    Change-Id: I295625007f9e69c4e29f0030bc8267f8c3439fcd

--- device.mk
+++ device.mk
@@ -136,6 +136,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qdcm_calib_data_mipi_mot_cmd_smd_1080p_549.xml:system/etc/qdcm_calib_data_mipi_mot_cmd_smd_1080p_549.xml
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-qdcm
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
