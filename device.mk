d168bf596581a9e24880bb24fb964407e22153f4
BtbN <btbn@btbn.de>
2018-08-13 19:46:37 +0200

    Update sensors
    Change-Id: Id17d28c81ead2cf055275c4dd123a6dcb053df34

--- device.mk
+++ device.mk
@@ -275,7 +275,9 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    libsensorndkbridge \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
